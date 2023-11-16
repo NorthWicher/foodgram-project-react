a2a9495391d1a7972574893fe5c86da92a849f15
Чистяков Никита <ChistyakovN@outlook.com>
2023-11-16 22:49:08 +0300

    testing flake8

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from api.views import (CustomUserViewSet,
-                       IngredientViewSet,
-                       RecipeViewSet,
+
+from api.views import (CustomUserViewSet, IngredientViewSet, RecipeViewSet,
                        TagViewSet)
 
 app_name = 'api'
