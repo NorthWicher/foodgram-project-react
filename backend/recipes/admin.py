a9ee6d01a3c0a5d71a10eee84a905694595b575d
Чистяков Никита <ChistyakovN@outlook.com>
2024-01-16 17:57:05 +0300

    fix test

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin 
+from django.contrib import admin
 from import_export.admin import ImportExportActionModelAdmin
 from recipes.models import (Favorite, Ingredient, Recipe,
                             ShoppingCart, Tag, IngredientAmount)
