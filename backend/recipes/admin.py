14623047de7d186625ed45bf7bf4c5a2fbf4a849
Чистяков Никита <ChistyakovN@outlook.com>
2024-01-16 16:48:08 +0300

    update BD

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin 
 from import_export.admin import ImportExportActionModelAdmin
 from recipes.models import (Favorite, Ingredient, Recipe,
                             ShoppingCart, Tag, IngredientAmount)
@@ -6,15 +6,22 @@ from users.models import Subscribe, User
 
 
 @admin.register(Ingredient)
-class RecipeIngredientAdmin(ImportExportActionModelAdmin):
-    list_display = ('name', 'measurement_unit')
-    list_filter = ('measurement_unit',)
+class IngredientAdmin(ImportExportActionModelAdmin):
+    list_display = (
+        'name',
+        'measurement_unit'
+    )
     search_fields = ('name',)
+    list_filter = ('measurement_unit',)
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ['name', 'color', 'slug']
+    list_display = [
+        'name',
+        'color',
+        'slug'
+    ]
     search_fields = ('name',)
     prepopulated_fields = {'slug': ('name',)}
 
@@ -30,17 +37,62 @@ class RecipeAdmin(admin.ModelAdmin):
         'id',
         'name',
         'author',
-        'pub_date',
-        'favorited_count',
+        'pub_date'
     )
-    list_filter = ('tags', 'pub_date')
-    search_fields = ('author__username', 'name')
+    search_fields = (
+        'author__username',
+        'author__email',
+        'name'
+    )
+    list_filter = (
+        'tags',
+        'pub_date'
+    )
 
-    def favorited_count(self, obj):
-        return obj.favorite_recipes.count()
 
-    favorited_count.short_description = 'Favorited Count'
-    inlines = [IngredientAmountInline]
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'user',
+        'recipe'
+    )
+    search_fields = (
+        'user__username',
+        'user__email',
+        'recipe__name'
+    )
+    list_filter = ('recipe__tags',)
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'user',
+        'recipe'
+    )
+    search_fields = (
+        'user__username',
+        'user__email',
+        'recipe__name'
+    )
+    list_filter = ('recipe__tags',)
+
+
+@admin.register(Subscribe)
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'user',
+        'following'
+    )
+    search_fields = (
+        'user__username',
+        'user__email',
+        'following__username',
+        'following__email'
+    )
 
 
 class UserAdmin(admin.ModelAdmin):
@@ -50,28 +102,16 @@ class UserAdmin(admin.ModelAdmin):
         'email',
         'first_name',
         'last_name',
-        'date_joined',
+        'date_joined'
     )
-    search_fields = ('email', 'username', 'first_name', 'last_name')
-    list_filter = ('date_joined', 'email', 'first_name')
+    search_fields = (
+        'email',
+        'username',
+        'first_name',
+        'last_name'
+    )
+    list_filter = ('date_joined',)
     empty_value_display = '-пусто-'
 
 
-@admin.register(Favorite)
-class FavoriteAdmin(admin.ModelAdmin):
-    list_display = (
-        'pk',
-        'user',
-        'recipe',
-        'pub_date',
-    )
-    search_fields = ('user__username',
-                     'user__email',
-                     'recipe__name')
-    list_filter = ('user',)
-    empty_value_display = '-пусто-'
-
-
-admin.site.register(ShoppingCart)
-admin.site.register(Subscribe)
 admin.site.register(User, UserAdmin)
